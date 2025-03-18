a967824d4cff52678f3999e37050392cf5d9fb04
Frederick2313072 <2031894837@qq.com>
2025-03-18 21:36:50 +0800

    3-18

--- loader.s
+++ loader.s
@@ -1,3 +1,14 @@
+    MAGIC_NUMBER    equ 0x1BADB002      ; define the magic number constant
+    ALIGN_MODULES   equ 0x00000001      ; tell GRUB to align modules
+
+    ; calculate the checksum (all options + checksum should equal 0)
+    CHECKSUM        equ -(MAGIC_NUMBER + ALIGN_MODULES)
+
+    section .text:                      ; start of the text (code) section
+    align 4                             ; the code must be 4 byte aligned
+        dd MAGIC_NUMBER                 ; write the magic number
+        dd ALIGN_MODULES                ; write the align modules instruction
+        dd CHECKSUM                     ; write the checksum
     global loader                   ; the entry symbol for ELF
 
     MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
